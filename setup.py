96781381a73d6b6bc3ed963fd5630e5958379a4f
Willi Rath <willirath@users.noreply.github.com>
2020-04-27 17:56:11 +0200

    Bump version for release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="esmvfc-cattools",
-    version="2020.04.26.1",
+    version="2020.04.27.1",
     description="ESM-VFC catalog tools",
     url="https://github.com/ESM-VFC/esm-vfc-catalogs",
     author="ESM-VFC developers",
