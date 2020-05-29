46ae65d991a76af24fabbeca19c83eb77fbad886
Willi Rath <wrath@geomar.de>
2020-05-29 13:07:58 +0200

    Add new DOI based download function and deprecate old one

--- esmvfc_cattools/fetching.py
+++ esmvfc_cattools/fetching.py
@@ -2,10 +2,85 @@ import os
 from pathlib import Path
 import pycurl
 from urllib.parse import urlparse
+import warnings
+
+from .aux import file_has_checksum
+
+
+def download_zenodo_files(
+    zenodo_doi, target_directory=None, force_download=False, filter_pattern=None
+):
+    """Download zenodo files for a given DOI.
+
+    Parameters
+    ----------
+    zenodo_doi : str
+        Zenodo DOI.  Example: "10.5281/zenodo.3819896"
+    target_directory : path or str
+        Target directory where all files will end up.
+    force_download : bool
+        Re-download and overwrite files even if they already exist?
+    filter_pattern : str
+        Pattern used to filter files.  Note that we use fnmatch and not regex.
+
+    Returns
+    -------
+    list of paths : all target files.
+
+    """
+    #     # check if we filter files
+    #     if filter_files is not None:
+    #         raise NotImplementedError("Filtering is not implemented yet")
+
+    # get zenodo record ID from doi
+    zenodo_record = zenodo_doi.split(".")[-1]
+    logging.debug(f"will download record {zenodo_record}")
+
+    # get full record from zenodo
+    # see https://developers.zenodo.org/#quickstart-upload for pointers
+    r = requests.get(f"https://zenodo.org/api/records/{zenodo_record}")
+    logging.debug(f"got status code {r.status_code}")
+    # should we debug-log the full json dump?
+
+    # TODO: Check that we got the correct DOI
+
+    # get list of source urls
+    filtered_files = list(
+        filter(lambda fn: fnmatch.fnmatch(fn["key"], filter_pattern), r.json()["files"])
+    )
+    all_urls = [file["links"]["self"] for file in filtered_files]
+    all_target_files = [
+        Path(target_directory) / Path(parsed_url.path).name
+        for parsed_url in map(urlparse, all_urls)
+    ]
+    all_checksums = [file["checksum"] for file in filtered_files]
+
+    # ensure target dir exists
+    Path(target_directory).mkdir(exist_ok=True, parents=True)
+
+    # download all wanted files with curl
+    for url, file, checksum in zip(all_urls, all_target_files, all_checksums):
+        if not file.exists() or force_download:
+            with open(file, "wb") as f:
+                logging.debug(f"will download {url} to {file}")
+                c = pycurl.Curl()
+                c.setopt(c.URL, url)
+                c.setopt(c.WRITEDATA, f)
+                c.perform()
+                c.close()
+                logging.debug(f"download of {url} to {file} done")
+        # This checks all files even if they were not downloaded:
+        if not file_has_checksum(file_name=file, checksum=checksum):
+            raise ValueError(f"Checksum for {file} does not match {checksum}")
+
+    return all_target_files
 
 
 def fetch_zenodo_data(catalog_entry, force_download=False):
     """Fetch data for `catalog_entry` from Zenodo.
+
+    WARNING: This function will be removed from future versions of
+    esmvfc_cattools. Use download_zenodo_files instead.
 
     Parameters
     ----------
@@ -19,6 +94,14 @@ def fetch_zenodo_data(catalog_entry, force_download=False):
         Defaults to `False`.
 
     """
+    warnings.warn(
+        (
+            "fetch_zenodo_data will be removed in future versions of esmvfc_cattools",
+            " use download_zenodo_files instead.",
+        ),
+        PendingDeprecationWarning,
+    )
+
     # set output directory and ensure it exists
     output_dir = Path(os.environ["ESM_VFC_DATA_DIR"]) / catalog_entry.cat.name
     output_dir.mkdir(parents=True, exist_ok=True)
